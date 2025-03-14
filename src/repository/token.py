ff81cd881ad17c7de57a2219160385cde88cd25d
Vladislab <vladb539@gmail.com>
2025-03-14 22:17:25 +0300

    add removin unused refresh tokens

--- src/repository/token.py
+++ src/repository/token.py
@@ -1,5 +1,6 @@
 from time import time
 from fastapi import Depends
+from sqlalchemy import delete, and_, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from secrets import token_urlsafe
 
@@ -14,12 +15,22 @@ class TokenRepository:
     ):
         self.session = session
 
+    async def remove_unused_tokens(self, user_id: int, user_agent: str) -> None:
+        stmt = delete(Token).where(
+            and_(
+                Token.user_id == user_id,
+                or_(Token.user_agent == user_agent, Token.exp <= time()),
+            )
+        )
+        await self.session.execute(stmt)
+
     async def create_refresh_token(
         self,
         user_id: int,
         user_agent: str,
         lifetime: int,
     ) -> str:
+        await self.remove_unused_tokens(user_id, user_agent)
         token = token_urlsafe(64)
         self.session.add(
             Token(
