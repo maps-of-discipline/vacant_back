40e472bcedb03eae8b55060ff0e8595b20c03563
Vladislab <vladb539@gmail.com>
2025-04-28 06:02:50 +0300

    Add python-multipart dependency and update permissions enum

--- src/api/applications/__init__.py
+++ src/api/applications/__init__.py
@@ -1,11 +1,11 @@
 from fastapi.routing import APIRouter
 
-router = APIRouter(prefix="/applications")
-
 from src.api.applications.transfer import router as transfer_router
 from src.api.applications.reinstatement import router as reinstatement_router
 from src.api.applications.change import router as change_router
 from src.api.applications.application import router as application_router
+
+router = APIRouter(prefix="/applications")
 
 router.include_router(transfer_router)
 router.include_router(reinstatement_router)
--- src/enums/auth.py
+++ src/enums/auth.py
@@ -1,13 +1,25 @@
-from enum import Enum
+from enum import Enum, StrEnum, auto
 
 
-class PermissionsEnum(str, Enum):
-    canCreateSelfApplication = "canCreateSelfApplication"
-    canViewOwnApplications = "canViewOwnApplications"
-    canCreateManySelfApplications = "canCreateManySelfApplications"
+class PermissionsEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values) -> str:
+        return name
 
-    canViewApplicationsList = "canViewApplicationsList"
-    canChangeApplicationStatus = "canChangeApplicationStatus"
+    canCreateUserComments = auto()
 
-    canViewStuffComments = "canViewStuffComments"
-    canCreateStuffComments = "canCreateStuffComments"
+    # user
+    canViewOwnApplications = auto()
+    canViewUserComments = auto()
+    canCreateSelfApplication = auto()
+    canEditOwnApplications = auto()
+
+    # stuff
+    canViewApplicationsList = auto()
+    canViewProcessApplication = auto()
+    canViewDashboard = auto()
+    canViewRups = auto()
+
+    canChangeApplicationStatus = auto()
+    canViewStuffComments = auto()
+    canCreateStuffComments = auto()
