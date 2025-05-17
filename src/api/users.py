9d453eec67e618a921f7e0b80069fde7139d1a3f
Vladislab <vladb539@gmail.com>
2025-05-17 20:57:29 +0300

    add users/me route

--- src/api/users.py
+++ src/api/users.py
@@ -3,8 +3,10 @@ from fastapi.routing import APIRouter
 
 
 from src.logger import get_logger
-from src.schemas.user import CreateUserSchema
+from src.schemas.user import CreateUserSchema, UserSchema
 from src.services.user import UserService
+from src.services.auth import PermissionRequire as Require
+from src.enums import PermissionsEnum as p
 
 logger = get_logger(__name__)
 
@@ -20,3 +22,8 @@ async def create_external_user(
     logger.info("Start handle user creation")
     await user_service.create_user(user)
     logger.info("Stop handle user creation")
+
+
+@router.post("/me")
+async def get_user_info(user: UserSchema = Depends(Require([]))) -> UserSchema:
+    return user
