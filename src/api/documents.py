3a1aafe16667773e5edf262c60869933945b3fc3
Vladislab <vladb539@gmail.com>
2025-05-29 07:26:49 +0300

    fix application pdf filename

--- src/api/documents.py
+++ src/api/documents.py
@@ -23,10 +23,16 @@ async def generate_transfer_document(
     encoded_filename = urllib.parse.quote(f"{user.shotname} Заявление на перевод.pdf")
     content_disposition = f"attachment; filename=\"{encoded_filename}\"; filename*=UTF-8''{encoded_filename}"
     pdf_bytes = pdf_content.getvalue()
+
+    headers = {
+        "Content-Disposition": content_disposition, 
+        "Access-Control-Expose-Headers": "Content-Disposition",
+    }
+
     return Response(
         content=pdf_bytes,
         media_type="application/octet-stream",
-        headers={"Content-Disposition": content_disposition},
+        headers=headers,
     )
 
 @router.get("/reinstatement/{application_id}")
@@ -38,14 +44,19 @@ async def generate_reinstatement_document(
     pdf_content = await document_service.generate_reinstatement_document(
         user, application_id
     )
-
     encoded_filename = urllib.parse.quote(f"{user.shotname} Заявление на восстановление.pdf")
     content_disposition = f"attachment; filename=\"{encoded_filename}\"; filename*=UTF-8''{encoded_filename}"
     pdf_bytes = pdf_content.getvalue()
+    
+    headers = {
+        "Content-Disposition": content_disposition, 
+        "Access-Control-Expose-Headers": "Content-Disposition",
+    }
+
     return Response(
         content=pdf_bytes,
         media_type="application/octet-stream",
-        headers={"Content-Disposition": content_disposition},
+        headers=headers,
     )
 
 @router.get("/change/{application_id}")
@@ -61,8 +72,14 @@ async def generate_change_document(
     encoded_filename = urllib.parse.quote(f"{user.shotname} Заявление на изменение условий обучения.pdf")
     content_disposition = f"attachment; filename=\"{encoded_filename}\"; filename*=UTF-8''{encoded_filename}"
     pdf_bytes = pdf_content.getvalue()
+    
+    headers = {
+        "Content-Disposition": content_disposition, 
+        "Access-Control-Expose-Headers": "Content-Disposition",
+    }
+
     return Response(
         content=pdf_bytes,
         media_type="application/octet-stream",
-        headers={"Content-Disposition": content_disposition},
+        headers=headers,
     )
